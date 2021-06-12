ee96db5ac8c00f681baa58622724313e5077a1d6
LiangJuntao990113 <ljt990113@mail.ustc.edu.cn>
2021-06-12 22:17:04 +0800

    modify 2.c

--- lab3/2.c
+++ lab3/2.c
@@ -125,38 +125,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    /*
-    //接受来自两个客户端的信息，只有两个客户端的请求都接受了(两个accept)才能继续执行下面的代码
-    //nc 127.0.0.1 6666可以看作是一个客户端使用了connect，即向服务端发起请求
-    int fd1 = accept(fd, NULL, NULL);
-    int fd2 = accept(fd, NULL, NULL);
-    if (fd1 == -1 || fd2 == -1)
-    {
-        perror("accept");
-        return 1;
-    }
-
-    //test
-    char str1[] = "Hello, client1.\n";
-    char str2[] = "Hello, client2.\n";
-    write(fd1, str1, sizeof(str1));
-    write(fd2, str2, sizeof(str2));
-
-    pthread_t thread1, thread2; //线程ID
-    struct Pipe pipe1;
-    struct Pipe pipe2;
-    pipe1.fd_send = fd1;
-    pipe1.fd_recv = fd2;
-    pipe2.fd_send = fd2;
-    pipe2.fd_recv = fd1;
-
-    //创建两个线程(两个客户端)，相当于服务端在两条路线上执行handle_chat函数
-    pthread_create(&thread1, NULL, handle_chat, (void *)&pipe1); //第四个参数为要传入handle_chat函数的参数
-    pthread_create(&thread2, NULL, handle_chat, (void *)&pipe2);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    */
-
     //TODO:利用多线程技术实现多人聊天室
     int userfd[MAX_CLIENT_NUM];
     pthread_t thread[MAX_CLIENT_NUM]; //最多支持32个用户同时在线
